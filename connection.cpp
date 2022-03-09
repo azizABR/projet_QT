509decfd05c56cdafc59fa7f4948874f8ee7f88c
aziz <aziz.abourgui@esprit.tn>
2022-03-09 13:51:01 -0800

    first commit

---
+++ connection.cpp
@@ -0,0 +1,19 @@
+#include "connection.h"
+
+Connection::Connection(){}
+
+bool Connection::createconnect()
+{
+db = QSqlDatabase::addDatabase("QODBC");
+bool test=false;
+db.setDatabaseName("Project");
+db.setUserName("abrougui");//inserer nom de l'utilisateur
+db.setPassword("esprit18");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+    return  test;
+}
+
+void Connection::closeConnection(){db.close();}
